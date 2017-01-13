d934dea7c8b3a055ef5a435d6c9c04b42842369e
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-13 17:19:30 +0300

    ..

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {RecipeService} from "./recipes/recipe.service";
+import {Subscription} from "rxjs/Rx";
 
 @Component({
   selector: 'rb-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onStore() {
+    this.subscription = this.recipeService.storeData().subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
+
+  onFetch() {
+    this.recipeService.fetchData();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
+import {Headers, Http, Response} from "@angular/http";
+import "rxjs/Rx";
 
 import {Recipe} from "./recipe";
 import {Ingredient} from "../shared/ingredient";
 
+
 @Injectable()
 export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg', [
       new Ingredient('French Fries', 2),
@@ -13,7 +18,8 @@ export class RecipeService {
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ];
 
-  constructor() { }
+  constructor(private http: Http) {
+  }
 
   getRecipes() {
     return this.recipes;
@@ -33,5 +39,26 @@ export class RecipeService {
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipebook-80b07.firebaseio.com/recipes.json', body, {headers: headers});
+  }
+
+  fetchData() {
+    return this.http.get('https://recipebook-80b07.firebaseio.com/recipes.json')
+      .map(
+        (response: Response) => response.json()
+      )
+      .subscribe(
+        (data: Recipe[]) => {
+          this.recipes = data;
+          this.recipesChanged.emit(this.recipes);
+        }
+      );
   }
 }
