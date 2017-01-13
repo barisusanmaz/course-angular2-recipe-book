3ca30e97b4a0ed2f3451055c8205073e92f81ae1
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-13 16:09:54 +0300

    ..

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import {Recipe} from "../recipe";
+import {RecipeService} from "../recipe.service";
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];   /*[
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];*/
+
+  //@Output() recipeSelected = new EventEmitter<Recipe>();
+  ////recipe = new Recipe('Dummy', 'Dummy', 'http://i.ebayimg.com/images/g/tKsAAOSwgY9Xf6m3/s-l1600.jpg');
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
+  onSelected(recipe: Recipe){
+    //this.recipeSelected.emit(recipe);
+  }
+
+}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+import {Recipe} from "./recipe";
+import {Ingredient} from "../shared/ingredient";
+
+@Injectable()
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg', [
+      new Ingredient('French Fries', 2),
+      new Ingredient('Pork Meat', 1)
+    ]),
+    new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return this.recipes;
+  }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
+  deleteRecipe(recipe: Recipe) {
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+    this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+}
