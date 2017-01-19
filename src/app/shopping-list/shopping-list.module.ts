988cfced24ce0984f5414427a54bd4ae73d22325
barisusanmaz <barisusanmaz@yahoo.com>
2017-01-19 15:53:10 +0300

    Feature module into app module

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,8 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from '@angular/forms';
 
-import {ShoppingListComponent} from '../shopping-list/shopping-list.component';
-import {ShoppingListAddComponent} from '../shopping-list/shopping-list-add.component';
+import {ShoppingListComponent} from './shopping-list.component';
+import {ShoppingListAddComponent} from './shopping-list-add.component';
 
 
 @NgModule({
